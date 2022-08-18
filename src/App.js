5bc421e24da472be4fd7bea11dabe024a2b55c30
Carl <qwerfcxzas1@gmail.com>
2022-08-18 23:11:19 +0800

    modify

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './app.scss'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as HashRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import SearchBus from './pages/SearchBus/SearchBus'
 
 function App() {
   return (
     <>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/searchBus" element={<SearchBus />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </>
   )
 }
